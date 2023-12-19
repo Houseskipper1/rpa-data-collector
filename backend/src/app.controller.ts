531877bbc651241fd61e9ab44726277afb35c07d
zinedine <zine.aitaider@yahoo.com>
2023-12-19 18:44:35 +0100

    csv export

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PappersService } from './scraping/entreprise/pappers.service';
-import { data } from 'cheerio/lib/api/attributes';
+import { EntrepriseService } from './entreprise/service/entreprise.service';
+import { Readable } from 'stream';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private _pappersService: PappersService,
-  ) {}
+  constructor(private readonly appService: AppService,
+              private readonly entrepriseService : EntrepriseService) {
+              }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('CSVExport')
+  @Header('Content-Type','text/plain')
+  @Header('Content-Disposition', 'attachment; filename=entrepriseData.csv')
+  async getFile(@Res() res) {
+    try {
+      const csvData = await this.entrepriseService.exportToCSV(); 
+      const textStream = new Readable();
+      textStream.push(csvData);
+      textStream.push(null); 
+      textStream.pipe(res);
+    } catch (error) {
+      res.status(500).send('Internal Server Error');
+    }
   }
 }
