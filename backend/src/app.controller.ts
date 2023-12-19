0781683e08024e5e7765097c0b7c67e8d8759327
Paul Gsell <paulgsell57@gmail.com>
2023-12-19 16:28:22 +0100

    Passage en mode service

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,16 +2,24 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PappersService } from './scraping/entreprise/pappers.service';
 import { data } from 'cheerio/lib/api/attributes';
+import { SocieteService } from './scraping/entreprise/societe.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private _pappersService: PappersService,
+    private _societeService: SocieteService,
   ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // get societe 
+  @Get('societe')
+  getSociete(): any {
+    return this._societeService.fetch();
   }
 }
--- backend/src/scraping/entreprise/societe.service.ts
+++ backend/src/scraping/entreprise/societe.service.ts
@@ -1,32 +1,41 @@
-const puppeteer = require('puppeteer');
-const resourceURL = 'https://www.societe.com/societe/';
-const testValue = "sarl-favata-338411101";
+import { Injectable } from '@nestjs/common';
+import { EntrepriseEntity } from 'src/entreprise/entities/entreprise.entity';
+import * as puppeteer from 'puppeteer';
 
-async function fetch(company_and_siren) {
+
+@Injectable()
+export class SocieteService {
+
+  private _entreprisesData: string[] = [
+    'sarl-favata-338411101',
+    'bati-france-57-851900654',
+    'lenninger-arthur-918347071',
+  ];
+  private _url = 'https://www.societe.com/societe/';
+
+  constructor() {}
+
+  async fetch() {
+    const entreprise = new EntrepriseEntity();
     const browser = await puppeteer.launch({headless: 'new'});
     const page = await browser.newPage();
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36");
     await page.setViewport({ width: 1366, height: 768});
-    console.log(`${resourceURL}${company_and_siren}.html`);
-    await page.goto(`${resourceURL}${company_and_siren}.html`);
-  
-    const element = await page.$('#rensjur') // Renseignements juridiques
-    const texte = await page.evaluate(element => element.innerText, element);
-
-    // les renseignements sont randomisés, donc on ne peut pas les scrapper
-    // le site est protégé contre le scrapping
-    // les balises sont intactes
-    // mais les données sont randomisées
+    console.log(`${this._url}${this._entreprisesData[0]}.html`);
+    await page.goto(`${this._url}${this._entreprisesData[0]}.html`);
+    
+    entreprise.name = this.getTdEval(page, 'Noms commerciaux');
 
     await browser.close();
 
-    return texte;
-}
+    return entreprise;
+  }
 
-fetch(testValue)
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.error(err.message);
-  });
+  async getTdEval(page, texteRecherche: string) {
+    return await page.$$eval('td', (tds: Element[], texteRecherche: string) => {
+      const td = tds.find(td => (td as HTMLElement).innerText.includes(texteRecherche));
+      return td ? (td as HTMLElement).nextElementSibling.innerHTML : null;
+    }, texteRecherche);
+  }
+
+}
