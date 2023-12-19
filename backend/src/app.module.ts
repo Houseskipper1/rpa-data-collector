af6f0a2d488ca573eab922b0fc1e3f7ec5f965f3
fxzoroark <fxzoroark@outlook.com>
2023-12-19 11:41:02 +0100

    Le Service SIRENE supporte la recherche via CSV

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { SirenService } from './api/siren/siren.service';
+import { SireneService } from './api/sirene/sirene.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, SirenService],
+  providers: [AppService, SireneService],
 })
 export class AppModule {}
