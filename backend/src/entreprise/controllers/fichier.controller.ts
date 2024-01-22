9e88065b828fad0b88c776e688e3f01e6dfc63a6
oussama476 <107109168+oussama476@users.noreply.github.com>
2024-01-22 11:50:57 +0100

    recuperation-fichier

---
+++ backend/src/entreprise/controllers/fichier.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { FichierService } from '../service/fichier.service';
+import { Fichier } from '../entities/fichier.entity';
+
+@Controller('fichiers')
+export class FichierController {
+    constructor(private fichierService: FichierService) {}
+
+    @Get()
+    async lireFichierCSV(
+        @Query('nombreLignes') nombreLignes: number = 5,
+    ): Promise<Fichier[]> {
+        return this.fichierService.lireFichierCSV(nombreLignes);
+    }
+
+}
